f865b7a067fc82449104034eb15590595681c9d9
LokeshPunarv <lokeshbs006@gmail.com>
2022-06-25 17:26:14 +0530

    TDD_Automation Day1

---
+++ MavenDemoProject/src/test/java/com/sgtesting/testcases/DemoTestCases.java
@@ -0,0 +1,31 @@
+package com.sgtesting.testcases;
+
+import org.testng.annotations.Test;
+
+public class DemoTestCases {
+	@Test
+	public void method1()
+	{
+		System.out.println("It Is a Method1 Statement...");
+	}
+	@Test
+	public void method2()
+	{
+		System.out.println("It Is a Method2 Statement...");
+	}
+	@Test
+	public void method3()
+	{
+		System.out.println("It Is a Method3 Statement...");
+	}
+	@Test
+	public void method4()
+	{
+		System.out.println("It Is a Method4 Statement...");
+	}
+	@Test
+	public void method5()
+	{
+		System.out.println("It Is a Method5 Statement...");
+	}
+}
